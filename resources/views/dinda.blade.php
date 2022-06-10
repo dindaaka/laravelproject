8f09c3ed451b3562c41bf154e95eafeceb580c8e
dhindakusherawati <dindaardianto2005@gmail.com>
2022-06-10 09:45:58 +0700

    24 mei 2022

---
+++ resources/views/dinda.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dinda</title>
+</head>
+<body>
+    <h1>Dhinda kusherawati A</h1>
+    <h1>nama saya dinda</h1>
+    <h1>hai salken</h1>
+
+</body>
+</html>
